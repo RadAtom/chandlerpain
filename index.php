b76d638545d7a3d213d6c6fd42a740f9adb7f117
Adam Visser <adamvissers@gmail.com>
2014-03-18 15:24:16 -0700

    getting started on changes wanted to make sure I didnt miss anything

--- index.php
+++ index.php
@@ -8,15 +8,26 @@ get_sidebar();
 ?>
 
 
-<div class="large-9 medium-8 small-12  columns">
+<div class="large-9 medium-8 small-12 columns">
 <section class="content page-blog landing" role="main">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<article class="">
+		<header class="post-header">
+			<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+			<span class="date-time">
+				<time pubdate>
+					Posted On<?php the_date(); ?>
+				</time>
+			</span>
+		</header>
 
 		<?php 
-		the_content(); 
+		the_excerpt(); 
 
-		?>
+		?> 
+		<footer class="post-footer small-12 columns ">
+			<?php comments_popup_link( '<span class="leave-reply right">' .'comment' . '</span>','<span class="leave-reply right">' .'comment' . '</span>','<span class="leave-reply right">' .'comment' . '</span>'); ?>
+		</footer>
 	</article>
 <?php
 endwhile;
